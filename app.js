a8c4224e68bfe05cae7a3e45282b0f50ec7f00f0
ahmedk1 <ahmed.katan89@gmail.com>
2021-07-28 15:33:59 +0300

    sign in with Google Auth

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 //jshint esversion:6
 //require("dotenv").config();
-//const express = require("express");
-//const https = require("https");
+
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-//const md5 = require("md5");
-//const bcrypt = require("bcrypt");
+
 ///
 const session = require("express-session");
 const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const findOrCreate = require("mongoose-findorcreate");
 
 const express = require("express");
 const app = express();
@@ -39,16 +39,42 @@ mongoose.set("useCreateIndex", true);
 const userSchema = new mongoose.Schema({
   email: String,
   password: String,
+  googleId: String,
 });
 
 userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
 const User = new mongoose.model("User", userSchema);
 
 //// local login setraticy
 passport.use(User.createStrategy());
 
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+passport.serializeUser(function (user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function (id, done) {
+  User.findById(id, function (err, user) {
+    done(err, user);
+  });
+});
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: CLIENT_ID,
+      clientSecret: CLIENT_SECRET,
+      callbackURL: "http://localhost:3000/auth/google/secrets",
+      userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo",
+    },
+    function (accessToken, refreshToken, profile, cb) {
+      console.log(profile);
+      User.findOrCreate({ googleId: profile.id }, function (err, user) {
+        return cb(err, user);
+      });
+    }
+  )
+);
 
 app.get("/", function (request, response) {
   response.render("home");
@@ -90,21 +116,21 @@ app.post("/register", (request, response) => {
     }
   );
 });
+app.get(
+  "/auth/google",
+  passport.authenticate("google", { scope: ["profile"] })
+);
+app.get(
+  "/auth/google/secrets",
+  passport.authenticate("google", { failureRedirect: "/login" }),
+  function (req, res) {
+    // Successful authentication, redirect home.
+    res.redirect("/secrets");
+  }
+);
 
 app.post("/login", (request, response) => {
-  const user = new User({
-    username: request.body.username,
-    password: request.body.password,
-  });
-  request.login(user, function (err) {
-    if (err) {
-      console.log(err);
-    } else {
-      passport.authenticate("local")(request, response, function () {
-        response.redirect("/secrets");
-      });
-    }
-  });
+  response.render("login");
 });
 
 app.listen(3000, function () {
